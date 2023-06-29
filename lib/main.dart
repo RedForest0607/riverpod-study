eb1889b4224e085400e1f524aaa2a3ffba4c1dca
Bizkit-1 <monolog3254@gmail.com>
2023-06-30 02:05:53 +0900

    🚧 load dashboard screen when history exists

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_study/common/db_helper.dart';
 import 'package:riverpod_study/common/routes.dart';
+import 'package:riverpod_study/model/event.dart';
+import 'package:riverpod_study/service/event_service.dart';
+import 'package:riverpod_study/view/dashboard/dashboard_screen.dart';
 import 'package:riverpod_study/view/startup/rule_screen.dart';
 
 void main() {
@@ -15,12 +20,29 @@ class TerraformingDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Terraforming Dashboard',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.yellow.shade900),
-      home: const RuleScreen(),
-      routes: routes,
+    return FutureBuilder<int>(
+      future: EventService.selectEventCount(),
+      builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+        if (snapshot.hasData) {
+          return MaterialApp(
+            title: 'Terraforming Dashboard',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(primaryColor: Colors.yellow.shade900),
+            home: snapshot.data == 0
+                ? const RuleScreen()
+                : const DashboardScreen(),
+            routes: routes,
+          );
+        } else {
+          return MaterialApp(
+            title: 'Terraforming Dashboard',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(primaryColor: Colors.yellow.shade900),
+            home: const RuleScreen(),
+            routes: routes,
+          );
+        }
+      },
     );
   }
 }
