510636aef4022f978b37766e26fd6cc9d7f0dea0
Bizkit-1 <monolog3254@gmail.com>
2023-06-30 01:32:05 +0900

    ⏪ restore main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_study/common/db_helper.dart';
 import 'package:riverpod_study/common/routes.dart';
-import 'package:riverpod_study/provider/event_provider.dart';
-import 'package:riverpod_study/service/event_service.dart';
-import 'package:riverpod_study/view/dashboard/dashboard_screen.dart';
 import 'package:riverpod_study/view/startup/rule_screen.dart';
 
 void main() {
@@ -15,19 +10,16 @@ void main() {
   runApp(const ProviderScope(child: TerraformingDashboard()));
 }
 
-class TerraformingDashboard extends HookConsumerWidget {
+class TerraformingDashboard extends StatelessWidget {
   const TerraformingDashboard({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    Future<int> eventCount = EventService.selectEventCount();
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Terraforming Dashboard',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.yellow.shade900),
-      home: eventCount.toString() == '0'
-          ? const RuleScreen()
-          : const DashboardScreen(),
+      home: const RuleScreen(),
       routes: routes,
     );
   }
